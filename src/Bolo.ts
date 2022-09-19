579ae6b61b58e30eb04a3db8c4f45a83d2d373f5
davi pimentel <davipimentel9@gmail.com>
2022-09-19 13:04:09 -0300

    Exemplo do Bolo

---
+++ src/Bolo.ts
@@ -0,0 +1,45 @@
+interface NewBolo {
+    tamanho: number;
+    sabor: string;
+    formato: string;
+    recheio: string;
+    cobertura: string;
+    valor: number;
+}
+
+class Bolo {
+    private tamanho: number;
+    private sabor: string;
+    private formato: string;
+    private recheio: string;
+    private cobertura: string;
+    private valor: number;
+    private qtdFatias: number = 0;
+
+    public get fatias () {
+        return this.qtdFatias
+    }
+
+    constructor ({tamanho,sabor,formato,recheio,cobertura,valor}: NewBolo) {
+        this.cobertura = cobertura;
+        this.formato = formato;
+        this.recheio = recheio;
+        this.tamanho = tamanho;
+        this.sabor = sabor;
+        this.valor = valor;
+    }
+      
+
+    public fatiar (qtdFatias: number) {
+         this.qtdFatias = qtdFatias;
+    }
+
+    public tirarFatia () {
+        //TODO: tirar uma fatia do bolo
+        this.qtdFatias--;
+    }
+       
+
+}
+
+export {Bolo}
